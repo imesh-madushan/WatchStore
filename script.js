fe8a1c90f5bdd56fd8f7ae500d0d4fcd6cebb76e
imeshme <imeshmadushan927@gmail.com>
2024-02-15 01:04:47 +0530

    mobile responsiveness done

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // This scripts only run after the document is fully loaded
 document.addEventListener("DOMContentLoaded", function() {
     var navbar = document.getElementById('nav');
+    var body = document.getElementById('body');
     var btnDropdown = document.getElementById('btnDropdown');
     var navDropdown = document.getElementById('navDropdown');
     var navHome = document.getElementById('navHome');
@@ -8,36 +9,69 @@ document.addEventListener("DOMContentLoaded", function() {
     var navRatings = document.getElementById('navRatings');
     var navServices = document.getElementById('navServices');
     var navContact = document.getElementById('navContact');
+    var mainContainer = document.getElementById('main-container');
+    var prodContainer = document.getElementById('products-container');
+    var leftBar = document.getElementById('left-bar');
     var catList = document.getElementById('c-list');
     var priceList = document.getElementById('p-list');
     var btnDropdownClicked = false;
     var navDropdownOpened = false;
 
+    // Event listener to listen every click
+    document.addEventListener('click', function(event) {
+        var clickedElement = event.target;
+        
+        // If the clicked button is not the dropdown button
+        if(clickedElement != btnDropdown)
+        {
+            if(navDropdown.classList.contains('show') == true)
+            {
+                navDropdown.classList.replace('show', 'hide');
+                navDropdownOpened = false;
+            }
+        }
+        else
+        {
+            
 
-    btnDropdown.onclick = function()
-    {   
-        btnDropdownClicked = true;
+            btnDropdownClicked = true;
 
-        // If dropdown bar is not shown mode or hide mode, dropdown bar will be shown
-        if(navDropdown.classList.contains('show') == false && navDropdown.classList.contains('hide') == false)
-        {
-            navDropdown.classList.add('show');
-            navDropdownOpened = true;
+            // If dropdown bar is not shown mode or hide mode, dropdown bar will be shown
+            if(navDropdown.classList.contains('show') == false && navDropdown.classList.contains('hide') == false)
+            {
+                if(window.innerWidth <= 400)
+                {
+                    leftBar.style.display = 'none';
+                    prodContainer.style.marginLeft = "-100px";
+                }
+                navDropdown.classList.add('show');
+                navDropdownOpened = true;
+            }
+            // If dropdown bar is shown mode, replace it with hide mode
+            else if(navDropdown.classList.contains('show') == true)
+            {
+                if(window.innerWidth <= 400)
+                {
+                    leftBar.style.display = 'grid';
+                    prodContainer.style.marginLeft = "-8%";
+                }
+                navDropdown.classList.replace('show', 'hide');
+                navDropdownOpened = false;
+            }
+            //if dropdown bar is hide mode, replace it with show mode
+            else if(navDropdown.classList.contains('hide') == true)
+            {
+                if(window.innerWidth <= 400)
+                {
+                    leftBar.style.display = 'none';
+                    prodContainer.style.marginLeft = "-100px";
+                }
+                navDropdown.classList.replace('hide', 'show');
+                navDropdownOpened = true;
+            }
         }
-        // If dropdown bar is shown mode, replace it with hide mode
-        else if(navDropdown.classList.contains('show') == true)
-        {
-            navDropdown.classList.replace('show', 'hide');
-            navDropdownOpened = false;
-        }
-        //if dropdown bar is hide mode, replace it with show mode
-        else if(navDropdown.classList.contains('hide') == true)
-        {
-            navDropdown.classList.replace('hide', 'show');
-            navDropdownOpened = true;
-        }
-    }
-    
+    });
+
     // When resizing the window
     window.addEventListener('resize', function(){
         // If window width is greater than 950 or less than 660, dropdown bar will be hidden
